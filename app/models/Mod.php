9936b59c6d21ea1cc505bc2915451da0413e19c7
Staz.IO <Staz@Staz.IO>
2017-04-27 17:29:38 -0400

    Added mod types.

--- app/models/Mod.php
+++ app/models/Mod.php
@@ -1,10 +1,25 @@
 <?php
 
 class Mod extends Eloquent {
+
+    const MOD_TYPE_UNIVERSAL = 0, MOD_TYPE_SERVER = 1, MOD_TYPE_CLIENT = 2;
+
 	public $timestamps = true;
 
 	public function versions()
 	{
 		return $this->hasMany('Modversion');
 	}
+
+    public function isUniversalMod() {
+        return $this->mod_type == Mod::MOD_TYPE_UNIVERSAL;
+    }
+
+    public function isServerMod() {
+        return $this->mod_type == Mod::MOD_TYPE_SERVER;
+    }
+
+    public function isClientMod() {
+        return $this->mod_type == Mod::MOD_TYPE_CLIENT;
+    }
 }
