f8c98f624223a91882b0b916c4585232ad859d46
Staz.IO <Staz@Staz.IO>
2017-04-27 18:14:06 -0400

    Added new migration for mod_type

---
+++ app/database/migrations/2017_04_27_171232_add_mod_type.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddModType extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('mods', function($table) {
+            $table->integer('mod_type')->default(0);
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('mods', function($table) {
+            $table->dropColumn('mod_type');
+        });
+	}
+
+}
