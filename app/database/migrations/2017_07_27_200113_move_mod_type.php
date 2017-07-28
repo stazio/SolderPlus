4d6225f6d7c3893b0e1cdf587e7c2b554a64a400
StazIO <staz@staz.io>
2017-07-27 21:05:00 -0400

    Made a new migration

--- app/database/migrations/2017_07_27_200113_move_mod_type.php
+++ app/database/migrations/2017_07_27_200113_move_mod_type.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddModType extends Migration {
+class MoveModType extends Migration {
 
 	/**
 	 * Run the migrations.
