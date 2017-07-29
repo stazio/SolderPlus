f37fd78d23b516cddb3d7a5d51ed6aefa0345490
StazIO <staz@staz.io>
2017-07-29 18:30:28 -0400

    Create InstallTest unit test

--- app/tests/InstallTest.php
+++ app/tests/InstallTest.php
@@ -6,30 +6,28 @@
  * Time: 7:42 PM
  */
 
-class InstallTest //extends TestCase
+class InstallTest extends TestCase
 {
-    public function setUp() {
-        InstallController::setStage(1);
-    }
+    const STAGES = 5;
 
-    public function tearDown()
-    {
-        InstallController::setStage(true);
-    }
-
-    public function testInstallRedirect() {
-        if (!InstallController::isInstalled()) {
+    public function testRedirects() {
+        for ($i = 1; $i <= self::STAGES; $i++) {
+            InstallController::setStage($i);
             $response = $this->call('GET', '/');
             $this->assertRedirectedTo('/install/stage' . InstallController::getStage());
         }
     }
 
-    public function testGetStage1() {
-        $response = $this->call('GET', '/install/stage1');
-        $this->assertResponseOk();
+    public function testGetStages() {
+        for ($i = 1; $i <= self::STAGES; $i++) {
+            InstallController::setStage(1);
+            $response = $this->call('GET', '/install/stage' . $i);
+            $this->assertResponseOk();
+        }
     }
 
     public function testSetStage1() {
+        InstallController::setStage(1);
         $driver = getenv('DB');
         $response = $this->call('POST', '/install/stage1', [
             'driver' => $driver,
@@ -44,83 +42,61 @@ class InstallTest //extends TestCase
     }
 
     /**
-     * @depends testGetStage1
+     * @depends testSetStage1
      */
-    public function testGetStage2() {
-        $response = $this->call('GET', '/install/stage2');
-        $this->assertResponseOk();
-    }
-
     public function testSetStage2() {
         $driver = getenv('DB');
-        $response = $this->call('POST', '/install/stage1', [
+        $response = $this->call('POST', '/install/stage2', [
             'app_url' => Config::get("app.url"),
-            'mod_url' => Config::get('solder.repo_location'),
+            'mod_uri' => Config::get('solder.repo_location'),
             'mirror_url' => Config::get('solder.mirror_url')
         ]);
         $this->assertResponseOk();
     }
 
     /**
-     * @depends testGetStage2
+     * @depends testSetStage2
      */
-    public function testGetStage3() {
-        $response = $this->call('GET', '/install/stage3');
-        $this->assertResponseOk();
-    }
-
     public function testSetStage3() {
         $driver = getenv('DB');
-        $response = $this->call('POST', '/install/stage1', [
-            'driver' => $driver,
-            'email'=> Config::get("database.connections.$driver.host"),
-            'username'=> Config::get("database.connections.$driver.database"),
-            'password'=> Config::get("database.connections.$driver.port"),
+        $response = $this->call('POST', '/install/stage3', [
+            'email' => 'admin@admin.com',
+            'username' =>'admin',
+            'password' => Hash::make('admin'),
         ]);
         $this->assertResponseOk();
     }
 
     /**
-     * @depends testGetStage3
+     * @depends testSetStage3
      */
-    public function testGetStage4() {
-        $response = $this->call('GET', '/install/stage4');
-        $this->assertResponseOk();
-    }
-
     public function testSetStage4() {
         $driver = getenv('DB');
-        $response = $this->call('POST', '/install/stage1', [
-            'driver' => $driver,
-            'host'=> Config::get("database.connections.$driver.host"),
-            'database'=> Config::get("database.connections.$driver.database"),
-            'port'=> Config::get("database.connections.$driver.port"),
-            'username'=> Config::get("database.connections.$driver.username"),
-            'password'=> Config::get("database.connections.$driver.password"),
-            'prefix' => Config::get("database.connections.$driver.prefix")
+        $response = $this->call('POST', '/install/stage4', [
+            'key' => 'sfIvEcNueZtwKsTAIYOIYng1iuPAgavJsfIvEcNueZtwKsTAIYOIYng1iuPAgavJ',
+            'name' => 'Test Key',
         ]);
         $this->assertResponseOk();
     }
 
     /**
-     * @depends testGetStage4
+     * @depends testSetStage4
      */
-    public function testGetStage5() {
-        $response = $this->call('GET', '/install/stage5');
+    public function testSetStage5() {
+        $driver = getenv('DB');
+        $response = $this->call('POST', '/install/stage5');
         $this->assertResponseOk();
     }
 
-    public function testSetStage5() {
-        $driver = getenv('DB');
-        $response = $this->call('POST', '/install/stage1', [
-            'driver' => $driver,
-            'host'=> Config::get("database.connections.$driver.host"),
-            'database'=> Config::get("database.connections.$driver.database"),
-            'port'=> Config::get("database.connections.$driver.port"),
-            'username'=> Config::get("database.connections.$driver.username"),
-            'password'=> Config::get("database.connections.$driver.password"),
-            'prefix' => Config::get("database.connections.$driver.prefix")
-        ]);
+    /**
+     * @depends testSetStage5
+     */
+    public function testSetStageDone()
+    {
+        $this->assertTrue(InstallController::isInstalled());
+        $this->assertTrue(InstallController::getStage() === true);
+        $this->be(User::find(1));
+        $this->call('GET', '/');
         $this->assertResponseOk();
     }
 }
