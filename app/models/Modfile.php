dba3c422680651fa2d41ea9dfe3383957dc34e1b
StazIO <staz@staz.io>
2017-07-30 13:08:13 -0400

    Fixed double slash

--- app/models/Modfile.php
+++ app/models/Modfile.php
@@ -9,13 +9,13 @@ class Modfile {
 
     public static function getModFolder() {
     	if (Cache::has('solder.mod_folder'))
-    		Cache::add('solder.mod_folder', Config::get('solder.repo_location') . '/mods/', 120);
-        return Cache::get('solder.mod_folder', Config::get('solder.repo_location') . '/mods/');
+    		Cache::add('solder.mod_folder', Config::get('solder.repo_location') . 'mods/', 120);
+        return Cache::get('solder.mod_folder', Config::get('solder.repo_location') . 'mods/');
     }
 
     public static function getModFolderURL() {
 	    if (Cache::has('solder.mirror_url'))
-		    Cache::add('solder.mirror_url', Config::get('solder.mirror_url') . '/mods/', 120);
-	    return Cache::get('solder.mirror_url', Config::get('solder.mirror_url') . '/mods/');
+		    Cache::add('solder.mirror_url', Config::get('solder.mirror_url') . 'mods/', 120);
+	    return Cache::get('solder.mirror_url', Config::get('solder.mirror_url') . 'mods/');
     }
 }
