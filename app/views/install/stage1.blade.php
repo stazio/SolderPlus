9113bb675772f16fdc37bd8cad4de2c59492326b
StazIO <staz@staz.io>
2017-07-29 21:48:22 -0400

    Cleaned up

--- app/views/install/stage1.blade.php
+++ app/views/install/stage1.blade.php
@@ -21,40 +21,40 @@
         </div>
 
         <div class="not-sqlite">
-        <div class="form-group" id="host-group">
-            <label for="host">
-                Database Host
-            </label>
-            <input class="form-control" name="host" id="host">
-        </div>
+            <div class="form-group" id="host-group">
+                <label for="host">
+                    Database Host
+                </label>
+                <input class="form-control" name="host" id="host">
+            </div>
 
-        <div class="form-group" id="database-group">
-            <label for="database">
-                Database Name
-            </label>
-            <input class="form-control" name="database" id="database">
-        </div>
+            <div class="form-group" id="database-group">
+                <label for="database">
+                    Database Name
+                </label>
+                <input class="form-control" name="database" id="database">
+            </div>
 
-        <div class="form-group" id="username-group">
-            <label for="username">
-                Database Username
-            </label>
-            <input class="form-control" name="username" id="username">
-        </div>
+            <div class="form-group" id="username-group">
+                <label for="username">
+                    Database Username
+                </label>
+                <input class="form-control" name="username" id="username">
+            </div>
 
-        <div class="form-group" id="password-group">
-            <label for="password">
-                Database Password
-            </label>
-            <input class="form-control" name="password" id="password">
-        </div>
+            <div class="form-group" id="password-group">
+                <label for="password">
+                    Database Password
+                </label>
+                <input class="form-control" name="password" id="password">
+            </div>
 
-        <div class="form-group" id="prefix-group">
-            <label for="prefix">
-                Database Prefix
-            </label>
-            <input class="form-control" name="prefix" id="prefix" value="solder_">
-        </div>
+            <div class="form-group" id="prefix-group">
+                <label for="prefix">
+                    Database Prefix
+                </label>
+                <input class="form-control" name="prefix" id="prefix" value="solder_">
+            </div>
         </div>
 @stop
 
