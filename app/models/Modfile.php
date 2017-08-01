563543ca6a32c21c51f7bfac666c37d561cfa213
StazIO <staz@staz.io>
2017-07-31 21:53:46 -0400

    Added a function to retrieve the mcmod.info file from mods.

--- app/models/Modfile.php
+++ app/models/Modfile.php
@@ -18,4 +18,31 @@ class Modfile {
 		    Cache::add('solder.mirror_url', Config::get('solder.mirror_url') . 'mods/', 120);
 	    return Cache::get('solder.mirror_url', Config::get('solder.mirror_url') . 'mods/');
     }
+
+    public static function getMcModInfo($path) {
+	    $zip = new ZipArchive();
+	    $zip2 = new ZipArchive();
+	    if ($zip->open($path)){
+		    for ($i = 0; $i < $zip->numFiles; $i++) {
+			    $name = $zip->getNameIndex($i);
+			    if (ends_with($name, '.jar')) {
+				    $file = $zip->getFromName($name);
+				    $path = explode('/', $name);
+				    array_pop($path);
+				    $path = implode('/', $path);
+				    if (!is_dir(storage_path('temp/'. $path)))
+					    mkdir(storage_path('temp/'. $path), 0777, true);
+				    file_put_contents(storage_path('temp/'. $name), $file);
+				    if ($zip2->open(storage_path('temp/'. $name)) === TRUE) {
+					    if ($mcmod = $zip2->getFromName('mcmod.info'))
+						    return json_decode($mcmod);
+				    }
+				    $zip2->close();
+			    }
+		    }
+	    }
+	    $zip2->close();
+	    $zip->close();
+	    return null;
+    }
 }
