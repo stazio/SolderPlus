316853c57f8335ba22a07f681933473c0038e0ee
StazIO <staz@staz.io>
2017-08-24 04:18:12 -0400

    Changed how version building works. Auto generated by Travis now.

---
+++ build-tools/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+$BRANCH=${TRAVIS_BRANCH}
+$VERSION=${TRAVIS_BUILD_NUMBER}
+
+composer create-build
+OUTPUT=app/version.php php build-version.php ${BRANCH} ${VERSION}
+curl -T latest-build/latest-build.zip ftp://${DEPLOY_USERNAME}:${DEPLOY_PASSWORD}@staz.io/${BRANCH}-${VERSION}.zip
