ca940f2f34a936b57f7b8ba5e1b20768c52efcad
StazIO <staz@staz.io>
2017-08-25 10:44:53 -0400

    Attempting to do deployment properly

--- build-tools/deploy.sh
+++ build-tools/deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+USERNAME=$DEPLOY_USERNAME
+PASSWORD=$DEPLOY_PASSWORD
+BRANCH=$TRAVIS_BRANCH
+VERSION=$TRAVIS_BUILD_NUMBER
 
 composer create-build
 curl -T latest-build/latest-build.zip ftp://${USERNAME}:${PASSWORD}@staz.io/${BRANCH}-${VERSION}.zip
