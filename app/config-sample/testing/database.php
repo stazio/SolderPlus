f0436c99620d7aefd66a9c7a59b7c69f269a1eea
StazIO <staz@staz.io>
2017-08-27 08:36:45 -0400

    Fixed issues with remote file testing and SQLite database testing.

--- app/config-sample/testing/database.php
+++ app/config-sample/testing/database.php
@@ -48,7 +48,7 @@ return array(
 
 		'sqlite' => array(
 			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
+			'database' => app_path('database/production.sqlite'),
 			'prefix'   => '',
 		),
 
